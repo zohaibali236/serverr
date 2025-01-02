4b8c00e3512aa951c337169d3a4fa598c86f1cda
Zohaib Ali <zohaib.ali236@gmail.com>
2025-01-03 01:07:24 +0500

    2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { ConfigService } from "@nestjs/config";
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
   constructor(
